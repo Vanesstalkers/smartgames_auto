477403db5f8f529e3944da03183b5882496c299c
VanesStalkers <ivan.mironov@bk.ru>
2026-03-22 02:11:46 +0300

    (+) configs.rules

---
+++ application/domain/game/configs/rules.js
@@ -0,0 +1,20 @@
+() => [
+  {
+    title: 'Автобизнес',
+    description: 'Колода для игр про продажи автомобилей',
+    pdfLinks: [
+      { label: 'Описание колоды', path: '/rules/deck.pdf' },
+      { label: 'Игра "Авто-продажи"', path: '/rules/sales.pdf' },
+      { label: 'Игра "Авто-аукцион"', path: '/rules/auction.pdf' },
+      { label: 'Игра "Авто-экспресс"', path: '/rules/express.pdf' },
+    ],
+    galleries: [
+      { label: 'Карты авто', selectGroup: 'car' },
+      { label: 'Карты сервисов', selectGroup: 'service' },
+      { label: 'Карты клиентов', selectGroup: 'client' },
+      { label: 'Карты особенностей', selectGroup: 'feature' },
+      { label: 'Карты кредитов', selectGroup: 'credit' },
+    ],
+  },
+];
+
